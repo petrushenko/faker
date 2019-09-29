b1ef7b1f79d70cd9e2a3b5fecfbbbd4995e8ab2f
petrushenko <igor.petrushenko1@gmail.com>
2019-09-29 16:42:21 +0300

    Added possibility to configure generation for class fields

--- Faker/Faker.cs
+++ Faker/Faker.cs
@@ -11,15 +11,26 @@ namespace Faker
 
         public Faker()
         {
+            _config = null;
             _generators = new Dictionary<Type, IGenerator>();
             _genericGeneratorFactory = new List<IGenericGeneratorFactory>();
             LoadGenerators();
             LoadGeneratorsFromDirectory();    
         }
+
+        public Faker(FakerConfig config)
+        {
+            _config = config;
+            _generators = new Dictionary<Type, IGenerator>();
+            _genericGeneratorFactory = new List<IGenericGeneratorFactory>();
+            LoadGenerators();
+            LoadGeneratorsFromDirectory();
+        }
         
         private readonly Dictionary<Type, IGenerator> _generators;
         private readonly List<IGenericGeneratorFactory> _genericGeneratorFactory;
         private readonly string _pluginPath = Path.Combine(Directory.GetCurrentDirectory(), "Plugins");
+        private FakerConfig _config;
 
         private void LoadGenerators()
         {
@@ -30,6 +41,8 @@ namespace Faker
             foreach (var type in types)
             {
                 if (type.FullName == null) continue;
+                if (type.GetInterfaces().Contains(typeof(IGenericGenerator))) continue;
+                if (!type.IsClass) continue;
                 if (currentAssembly.CreateInstance(type.FullName) is IGenerator generatorPlugin)
                 {
                     var generatorType = generatorPlugin.GetGenerationType();
@@ -64,6 +77,8 @@ namespace Faker
                 foreach (var type in types)
                 {
                     if (type.FullName == null) continue;
+                    if (type.GetInterfaces().Contains(typeof(IGenericGenerator))) continue;
+                    if (!type.IsClass) continue;
                     if (assembly.CreateInstance(type.FullName) is IGenerator generatorPlugin)
                     {
                         var generatorType = generatorPlugin.GetGenerationType();
@@ -107,9 +122,9 @@ namespace Faker
             {
                 return generator.Generate();
             }
-            if (type.IsValueType)
+            if (type.IsValueType || type.IsClass)
             {
-                var instance = Activator.CreateInstance(type);
+                var instance = InitializeWithConstructor(type);
                 FillObject(instance);
                 return instance;
             }
@@ -121,20 +136,13 @@ namespace Faker
                     return genericGenerator.Generate();
                 }
             }
-
-            if (type.IsClass)
-            {
-                var instance = InitializeWithConstructor(type);
-                FillObject(instance);
-                return instance;
-            }
             
             return Activator.CreateInstance(type);
         }
 
-        private Dictionary<Type, IGenericGenerator> CreateGenericGenerators(Type type)
+        private Dictionary<Type, IGenerator> CreateGenericGenerators(Type type)
         {
-            var generators = new Dictionary<Type, IGenericGenerator>();
+            var generators = new Dictionary<Type, IGenerator>();
             foreach (var generatorFactory in _genericGeneratorFactory)
             {
                 var generator = generatorFactory.GetGenerator(type.GetGenericArguments());
@@ -153,17 +161,33 @@ namespace Faker
             foreach (var property in properties)
             {
                 if (!property.CanWrite) continue;
-                var propertyType = property.PropertyType;
-                var value = GenerateValue(propertyType, propertyType == instance.GetType());
-                property.SetValue(instance, value);
+                var generator = _config.GetGeneratorByMemberInfo(property);
+                if (generator != null)
+                {
+                    property.SetValue(instance, generator.Generate());
+                }
+                else
+                {
+                    var propertyType = property.PropertyType;
+                    var value = GenerateValue(propertyType, propertyType == instance.GetType());
+                    property.SetValue(instance, value);
+                }
             }
             var fieldInfos = new List<FieldInfo>(type.GetFields());
             foreach (var fieldInfo in fieldInfos)
             {
                 if (fieldInfo.IsLiteral) continue;
-                var fieldType = fieldInfo.FieldType;
-                var value = GenerateValue(fieldType, fieldType == instance.GetType());
-                fieldInfo.SetValue(instance, value);
+                var generator = _config.GetGeneratorByMemberInfo(fieldInfo);
+                if (generator != null)
+                {
+                    fieldInfo.SetValue(instance, generator.Generate());
+                }
+                else
+                {
+                    var fieldType = fieldInfo.FieldType;
+                    var value = GenerateValue(fieldType, fieldType == instance.GetType());
+                    fieldInfo.SetValue(instance, value);
+                }
             }
         }
 
@@ -187,15 +211,24 @@ namespace Faker
         {
             var constructorInfos = type.GetConstructors();
             var constructorInfo = GetConstructorWithMaxNumberOfParameters(constructorInfos);
-            if (constructorInfo == null) return null;
+            if (constructorInfo == null) return Activator.CreateInstance(type);
             
             var constructorParameters = new List<object>();
             var parametersInfo = constructorInfo.GetParameters();
             foreach (var parameterInfo in parametersInfo)
             {
-                var parameterType = parameterInfo.ParameterType;
-                var parameter = GenerateValue(parameterType, parameterType == type);
-                constructorParameters.Add(parameter);
+                string parameterName = parameterInfo.Name;
+                IGenerator generator = _config.GetGeneratorByName(parameterName);
+                if (generator != null)
+                {
+                    constructorParameters.Add(generator.Generate());
+                }
+                else
+                {
+                    var parameterType = parameterInfo.ParameterType;
+                    var parameter = GenerateValue(parameterType, parameterType == type);
+                    constructorParameters.Add(parameter);
+                }
                 
             }
             var instance = constructorInfo.Invoke(constructorParameters.ToArray());
---
+++ Faker/FakerConfig.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Threading.Tasks;
+
+namespace Faker
+{
+    public class FakerConfig
+    {
+        private Dictionary<MemberInfo, IGenerator> Generators;
+
+        internal IGenerator GetGeneratorByName(string parameterName)
+        {
+            foreach (var member in Generators.Keys)
+            {
+                if (member.Name.Equals(parameterName, StringComparison.OrdinalIgnoreCase))
+                {
+                    Generators.TryGetValue(member, out var generator);
+                    return generator;
+                }
+            }
+
+            return null;
+        }
+
+        internal IGenerator GetGeneratorByMemberInfo(MemberInfo memberInfo)
+        {
+            Generators.TryGetValue(memberInfo, out var generator);
+            return generator;
+        }
+
+        public FakerConfig()
+        {
+            Generators = new Dictionary<MemberInfo, IGenerator>();
+        }
+
+        public void Add<TClass, TMember, TGenerator>(Expression<Func<TClass, TMember>> expressionTree)
+        where TGenerator: IGenerator
+        {
+            var generator = (IGenerator)Activator.CreateInstance<TGenerator>();
+
+            var body = (MemberExpression)expressionTree.Body;
+            var memberInfo = body.Member;
+            Generators.Add(memberInfo, generator);
+        }
+    }
+}
