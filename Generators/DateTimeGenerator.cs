65d2f05a37a4d7e92fe1cb6af524257ba5e6ee2b
petrushenko <igor.petrushenko1@gmail.com>
2019-09-29 22:22:53 +0300

    Added generators

---
+++ Generators/DateTimeGenerator.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Faker;
+
+namespace Generators
+{
+    public class DateTimeGenerator : IGenerator
+    {
+        private static readonly Random Random = new Random((int)DateTime.Now.Ticks & 0x0000FFFF);
+        public object Generate()
+        {
+            var buf = new byte[8];
+            Random.NextBytes(buf);
+            var ticks = BitConverter.ToInt64(buf, 0);
+            return new DateTime(Math.Abs(ticks % (DateTime.MaxValue.Ticks - DateTime.MinValue.Ticks)) + DateTime.MinValue.Ticks);
+        }
+
+        public Type GetGenerationType()
+        {
+            return typeof(DateTime);
+        }
+    }
+}
---
+++ Generators/FloatGenerator.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Faker;
+
+namespace Generators
+{
+    public class FloatGenerator : IGenerator
+    {
+        private static readonly Random Random = new Random();
+        public object Generate()
+        {
+            double mantissa = (Random.NextDouble() * 2.0) - 1.0;
+            // choose -149 instead of -126 to also generate subnormal floats (*)
+            double exponent = Math.Pow(2.0, Random.Next(-126, 128));
+            return (float)(mantissa * exponent);
+        }
+
+        public Type GetGenerationType()
+        {
+            return typeof(float);
+        }
+    }
+}
