7fc28a53f66d85a3ada5a4378e25de5a70cee80f
petrushenko <igor.petrushenko1@gmail.com>
2019-09-30 23:38:15 +0300

    Added unit tests

---
+++ Faker.Tests/FakerTests.cs
@@ -0,0 +1,91 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+
+namespace Faker.Tests
+{
+    [TestClass]
+    public class FakerTests
+    {
+        private readonly Faker _faker = new Faker();
+        [TestMethod]
+        public void ListTest()
+        {
+            var faker = new Faker();
+
+            var list = faker.Create<List<int>>() as List<int>;
+
+            Assert.IsNotNull(list);
+            Assert.AreNotEqual(list, default);
+        }
+
+        private enum MyEnum
+        {
+            One,
+            Two,
+            Three
+        }
+
+        [TestMethod]
+        public void InnerClass()
+        {
+            var myObject = _faker.Create<MyClass>() as MyClass;
+
+            Assert.IsNotNull(myObject);
+            Assert.IsNull(myObject.InnerClass.MyClass);
+            Assert.AreNotEqual(myObject.PublicField, default);
+            Assert.AreNotEqual(myObject.PublicProperty, default);
+        }
+
+        [TestMethod]
+        public void ClassTest()
+        {
+            var myObject = _faker.Create<MyClass>() as MyClass;
+
+            Assert.IsNotNull(myObject);
+            Assert.AreNotEqual(myObject, default);
+        }
+
+        [TestMethod]
+        public void DefaultInt()
+        {
+            var int32Value = _faker.Create<int>();
+            Assert.AreNotEqual(int32Value, default);
+        }
+
+        [TestMethod]
+        public void DefaultBool()
+        {
+            var boolValue = _faker.Create<bool>();
+            Assert.AreNotEqual(boolValue, default);
+        }
+
+        [TestMethod]
+        public void DefaultDouble()
+        {
+            var doubleValue = _faker.Create<double>();
+            Assert.AreNotEqual(doubleValue, default);
+        }
+
+        [TestMethod]
+        public void DefaultEnum()
+        {
+            var enumValue = _faker.Create<MyEnum>();
+            Assert.AreNotEqual(enumValue, default);
+        }
+
+        [TestMethod]
+        public void DefaultLong()
+        {
+            var longValue = _faker.Create<long>();
+            Assert.AreNotEqual(longValue, default);
+        }
+
+        [TestMethod]
+        public void DefaultString()
+        {
+            var stringValue = _faker.Create<string>();
+            Assert.AreNotEqual(stringValue, default);
+        }
+
+    }
+}
---
+++ Faker.Tests/MyClass.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Faker.Tests
+{
+    public class MyClass
+    {
+        private int _privateField;
+        private int _privateProperty { get; }
+
+        public int PublicField;
+        public int PublicProperty { get; }
+        public MyClass InnerSameClass { get; set; }
+        public MyOtherClass InnerClass { get; set; }
+        public MyClass(int privateProperty, int privateField, int publicField, int publicProperty)
+        {
+            _privateProperty = privateProperty;
+            _privateField = privateField;
+            PublicField = publicField;
+            PublicProperty = publicProperty;
+        }
+    }
+}
